3a956cb7fd10bfbd6ae0bf4fe200e1218a01f626
Dmitry Tur <dmitriy.tur.v@gmail.com>
2018-06-12 09:39:54 +0300

    Save

--- app/src/main/java/ua/nure/queue_managment_android/constants/Constants.java
+++ app/src/main/java/ua/nure/queue_managment_android/constants/Constants.java
@@ -2,5 +2,5 @@ package ua.nure.queue_managment_android.constants;
 
 public interface Constants {
 
-    String BASE_URL = "http://192.168.0.105:8085";
+    String BASE_URL = "http://192.168.137.1:8085";
 }
